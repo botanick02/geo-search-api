3b4a616bbabd29c59e7f6186da3cff08138fcf4c
Danyil Tuhaienko <botanick02@gmail.com>
2024-01-08 19:14:43 +0200

    feat: added test api endpoint

---
+++ Controllers/GeoSearchController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoSearchApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeoSearchController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<List<string>> SearchCityName(string cityName)
+        {
+            return new List<string> { "Kyiv!!!" };
+        }
+    }
+}
